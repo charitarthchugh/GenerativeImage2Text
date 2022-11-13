9bdbd8e2602949a8e43f08b02a9d162b25728674
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-11-13 05:44:30 -0500

    Add return statement for one image inference

--- generativeimage2text/inference.py
+++ generativeimage2text/inference.py
@@ -25,7 +25,6 @@ from .process_image import load_image_by_pil
 from .model import get_git_model
 
 
-
 class MinMaxResizeForTest(object):
     def __init__(self, min_size, max_size):
         self.min_size = min_size
@@ -54,22 +53,22 @@ class MinMaxResizeForTest(object):
         return (oh, ow)
 
     def __repr__(self):
-        return 'MinMaxResizeForTest({}, {})'.format(
-            self.min_size, self.max_size)
+        return "MinMaxResizeForTest({}, {})".format(self.min_size, self.max_size)
 
     def __call__(self, img):
         size = self.get_size(img.size)
         import torchvision.transforms.functional as F
+
         image = F.resize(img, size, interpolation=PIL.Image.BICUBIC)
         return image
 
 
 def test_git_inference_single_image(image_path, model_name, prefix):
     param = {}
-    if File.isfile(f'aux_data/models/{model_name}/parameter.yaml'):
-        param = load_from_yaml_file(f'aux_data/models/{model_name}/parameter.yaml')
+    if File.isfile(f"aux_data/models/{model_name}/parameter.yaml"):
+        param = load_from_yaml_file(f"aux_data/models/{model_name}/parameter.yaml")
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
     if isinstance(image_path, str):
         image_path = [image_path]
     # if it is more than 1 image, it is normally a video with multiple image
@@ -81,8 +80,8 @@ def test_git_inference_single_image(image_path, model_name, prefix):
 
     # model
     model = get_git_model(tokenizer, param)
-    pretrained = f'output/{model_name}/snapshot/model.pt'
-    checkpoint = torch_load(pretrained)['model']
+    pretrained = f"output/{model_name}/snapshot/model.pt"
+    checkpoint = torch_load(pretrained)["model"]
     load_state_dict(model, checkpoint)
     model.cuda()
     model.eval()
@@ -90,55 +89,59 @@ def test_git_inference_single_image(image_path, model_name, prefix):
 
     # prefix
     max_text_len = 40
-    prefix_encoding = tokenizer(prefix,
-                                padding='do_not_pad',
-                                truncation=True,
-                                add_special_tokens=False,
-                                max_length=max_text_len)
-    payload = prefix_encoding['input_ids']
+    prefix_encoding = tokenizer(
+        prefix,
+        padding="do_not_pad",
+        truncation=True,
+        add_special_tokens=False,
+        max_length=max_text_len,
+    )
+    payload = prefix_encoding["input_ids"]
     if len(payload) > max_text_len - 2:
-        payload = payload[-(max_text_len - 2):]
+        payload = payload[-(max_text_len - 2) :]
     input_ids = [tokenizer.cls_token_id] + payload
 
     with torch.no_grad():
-        result = model({
-            'image': img,
-            'prefix': torch.tensor(input_ids).unsqueeze(0).cuda(),
-        })
-    cap = tokenizer.decode(result['predictions'][0].tolist(), skip_special_tokens=True)
-    logging.info('output: {}'.format(cap))
-    print(cap)
+        result = model(
+            {
+                "image": img,
+                "prefix": torch.tensor(input_ids).unsqueeze(0).cuda(),
+            }
+        )
+    cap = tokenizer.decode(result["predictions"][0].tolist(), skip_special_tokens=True)
+    logging.info("output: {}".format(cap))
     return cap
 
+
 def get_image_transform(param):
-    crop_size = param.get('test_crop_size', 224)
-    if 'test_respect_ratio_max' in param:
-        trans = [
-            MinMaxResizeForTest(crop_size, param['test_respect_ratio_max'])
-        ]
+    crop_size = param.get("test_crop_size", 224)
+    if "test_respect_ratio_max" in param:
+        trans = [MinMaxResizeForTest(crop_size, param["test_respect_ratio_max"])]
     else:
         trans = [
             Resize(crop_size, interpolation=Image.BICUBIC),
             CenterCrop(crop_size),
             lambda image: image.convert("RGB"),
-
         ]
-    trans.extend([
-        ToTensor(),
-        Normalize(
-            (0.48145466, 0.4578275, 0.40821073),
-            (0.26862954, 0.26130258, 0.27577711),
-        ),
-    ])
+    trans.extend(
+        [
+            ToTensor(),
+            Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
     transforms = Compose(trans)
     return transforms
 
+
 def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
     param = {}
-    if File.isfile(f'output/{model_name}/parameter.yaml'):
-        param = load_from_yaml_file(f'output/{model_name}/parameter.yaml')
+    if File.isfile(f"output/{model_name}/parameter.yaml"):
+        param = load_from_yaml_file(f"output/{model_name}/parameter.yaml")
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
     image_tsv = TSVFile(image_tsv)
     question_tsv = TSVFile(question_tsv) if question_tsv else None
 
@@ -146,8 +149,8 @@ def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
 
     # model
     model = get_git_model(tokenizer, param)
-    pretrained = f'output/{model_name}/snapshot/model.pt'
-    checkpoint = torch_load(pretrained)['model']
+    pretrained = f"output/{model_name}/snapshot/model.pt"
+    checkpoint = torch_load(pretrained)["model"]
     load_state_dict(model, checkpoint)
     model.eval()
 
@@ -158,8 +161,10 @@ def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
     max_text_len = 40
     rank = get_mpi_rank()
     world_size = get_mpi_size()
+
     def get_rank_specific_tsv(rank):
-        return '{}.{}.{}.tsv'.format(out_tsv, rank, world_size)
+        return "{}.{}.{}.tsv".format(out_tsv, rank, world_size)
+
     if world_size > 1:
         curr_out_tsv = get_rank_specific_tsv(rank)
     else:
@@ -171,8 +176,9 @@ def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
     curr_end = min(curr_end, total)
 
     if question_tsv:
+
         def gen_rows():
-            for i  in tqdm(range(curr_start, curr_end)):
+            for i in tqdm(range(curr_start, curr_end)):
                 image_key, image_col = image_tsv[i]
                 q_key, q_info = question_tsv[i]
                 assert image_key == q_key
@@ -181,37 +187,50 @@ def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
                 img = transforms(img)
                 img = img.cuda().unsqueeze(0)
                 for q in q_info:
-                    prefix = q['question']
-                    prefix_encoding = tokenizer(prefix,
-                                                padding='do_not_pad',
-                                                truncation=True,
-                                                add_special_tokens=False,
-                                                max_length=max_text_len)
-                    payload = prefix_encoding['input_ids']
+                    prefix = q["question"]
+                    prefix_encoding = tokenizer(
+                        prefix,
+                        padding="do_not_pad",
+                        truncation=True,
+                        add_special_tokens=False,
+                        max_length=max_text_len,
+                    )
+                    payload = prefix_encoding["input_ids"]
                     if len(payload) > max_text_len - 2:
-                        payload = payload[-(max_text_len - 2):]
+                        payload = payload[-(max_text_len - 2) :]
                     input_ids = [tokenizer.cls_token_id] + payload
                     with torch.no_grad():
-                        result = model({
-                            'image': img,
-                            'prefix': torch.tensor(input_ids).unsqueeze(0).cuda(),
-                        })
-                    answer = tokenizer.decode(result['predictions'][0].tolist(), skip_special_tokens=True)
-                    result = {'answer': answer, 'question_id': q['question_id']}
+                        result = model(
+                            {
+                                "image": img,
+                                "prefix": torch.tensor(input_ids).unsqueeze(0).cuda(),
+                            }
+                        )
+                    answer = tokenizer.decode(
+                        result["predictions"][0].tolist(), skip_special_tokens=True
+                    )
+                    result = {"answer": answer, "question_id": q["question_id"]}
                     yield json_dump(result),
+
     else:
+
         def gen_rows():
-            for i  in tqdm(range(curr_start, curr_end)):
+            for i in tqdm(range(curr_start, curr_end)):
                 key, col = image_tsv[i]
                 img = pilimg_from_base64(col)
                 img = transforms(img)
                 img = img.cuda().unsqueeze(0)
                 with torch.no_grad():
-                    result = model({
-                        'image': img,
-                    })
-                cap = tokenizer.decode(result['predictions'][0].tolist(), skip_special_tokens=True)
-                yield key, json_dump([{'caption': cap}])
+                    result = model(
+                        {
+                            "image": img,
+                        }
+                    )
+                cap = tokenizer.decode(
+                    result["predictions"][0].tolist(), skip_special_tokens=True
+                )
+                yield key, json_dump([{"caption": cap}])
+
     tsv_writer(gen_rows(), curr_out_tsv)
     if world_size > 1 and rank == 0:
         all_sub_tsv = [get_rank_specific_tsv(i) for i in range(world_size)]
@@ -221,17 +240,20 @@ def test_git_inference_single_tsv(image_tsv, model_name, question_tsv, out_tsv):
                 break
             else:
                 import time
-                logging.info('waiting {}'.format(','.join(not_ready)))
+
+                logging.info("waiting {}".format(",".join(not_ready)))
                 time.sleep(5)
         from .tsv_io import concat_tsv_files
+
         concat_tsv_files(all_sub_tsv, out_tsv)
+
 
 def convert_tsv_to_vqa_json(predict_file, out_json):
     result = [json.loads(s) for s, in tsv_reader(predict_file)]
     write_to_file(json_dump(result), out_json)
 
-def convert_tsv_to_coco_format(res_tsv, outfile,
-        sep='\t', key_col=0, cap_col=1):
+
+def convert_tsv_to_coco_format(res_tsv, outfile, sep="\t", key_col=0, cap_col=1):
     results = []
     with open(res_tsv) as fp:
         for line in fp:
@@ -240,58 +262,54 @@ def convert_tsv_to_coco_format(res_tsv, outfile,
             if cap_col < len(parts):
                 caps = json.loads(parts[cap_col])
                 if len(caps) == 0:
-                    caps = [{'caption': ''}]
-                assert len(caps) == 1, 'cannot evaluate multiple captions per image'
-                cap = caps[0]['caption']
+                    caps = [{"caption": ""}]
+                assert len(caps) == 1, "cannot evaluate multiple captions per image"
+                cap = caps[0]["caption"]
             else:
                 # empty caption generated
                 cap = ""
-            results.append(
-                    {'image_id': key,
-                    'caption': cap}
-                    )
-    with open(outfile, 'w') as fp:
+            results.append({"image_id": key, "caption": cap})
+    with open(outfile, "w") as fp:
         json.dump(results, fp)
+
 
 def iter_caption_to_json(iter_caption, json_file):
     # save gt caption to json format so thet we can call the api
     key_captions = [(key, json.loads(p)) for key, p in iter_caption]
 
     info = {
-        'info': 'dummy',
-        'licenses': 'dummy',
-        'type': 'captions',
+        "info": "dummy",
+        "licenses": "dummy",
+        "type": "captions",
     }
-    info['images'] = [{'file_name': k, 'id': k} for k, _ in key_captions]
+    info["images"] = [{"file_name": k, "id": k} for k, _ in key_captions]
     n = 0
     annotations = []
     for k, cs in key_captions:
         for c in cs:
-            annotations.append({
-                'image_id': k,
-                'caption': c['caption'],
-                'id': n
-            })
+            annotations.append({"image_id": k, "caption": c["caption"], "id": n})
             n += 1
-    info['annotations'] = annotations
+    info["annotations"] = annotations
     write_to_file(json.dumps(info), json_file)
 
-def evaluate_on_coco_caption(res_file, label_file, outfile=None,
-                             ):
-    if not outfile:
-        outfile = op.splitext(res_file)[0] + '.eval.json'
 
-    if res_file.endswith('.tsv'):
-        res_file_coco = op.splitext(res_file)[0] + '_coco_format.json'
+def evaluate_on_coco_caption(
+    res_file,
+    label_file,
+    outfile=None,
+):
+    if not outfile:
+        outfile = op.splitext(res_file)[0] + ".eval.json"
+
+    if res_file.endswith(".tsv"):
+        res_file_coco = op.splitext(res_file)[0] + "_coco_format.json"
         convert_tsv_to_coco_format(res_file, res_file_coco)
     else:
         res_file_coco = res_file
 
-    if label_file.endswith('.tsv'):
-        json_caption = '/tmp/{}'.format(label_file)
-        iter_caption_to_json(
-            TSVFile(label_file),
-            json_caption)
+    if label_file.endswith(".tsv"):
+        json_caption = "/tmp/{}".format(label_file)
+        iter_caption_to_json(TSVFile(label_file), json_caption)
         label_file = json_caption
 
     from pycocotools.coco import COCO
@@ -301,7 +319,7 @@ def evaluate_on_coco_caption(res_file, label_file, outfile=None,
     cocoRes = coco.loadRes(res_file_coco)
     cocoEval = COCOEvalCap(coco, cocoRes)
 
-    cocoEval.params['image_id'] = cocoRes.getImgIds()
+    cocoEval.params["image_id"] = cocoRes.getImgIds()
 
     # evaluate results
     # SPICE will take a few minutes the first time, but speeds up due to caching
@@ -310,15 +328,15 @@ def evaluate_on_coco_caption(res_file, label_file, outfile=None,
     if not outfile:
         print(result)
     else:
-        with open(outfile, 'w') as fp:
+        with open(outfile, "w") as fp:
             json.dump(result, fp, indent=4)
     return result
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init_logging()
     kwargs = parse_general_args()
-    logging.info('param:\n{}'.format(pformat(kwargs)))
-    function_name = kwargs['type']
-    del kwargs['type']
+    logging.info("param:\n{}".format(pformat(kwargs)))
+    function_name = kwargs["type"]
+    del kwargs["type"]
     locals()[function_name](**kwargs)
-
